13b32ea63b8f522fe1a3f3c676f669ff4856b716
Chuudim <chulanovdima2507@gmail.com>
2023-03-09 21:46:54 +0200

    ДЗ фул

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,13 +1,14 @@
 import { galleryItems } from './gallery-items.js';
 
+
 const gallery = document.querySelector('.gallery');
 
 function createGalleryItem({ preview, original, description }) {
   return `
-    <a class="gallery__item" href="${original}">
+    <li><a class="gallery__item" href="${original}">
       <img class="gallery__image" src="${preview}" alt="${description}" />
       <div class="gallery__caption">${description}</div>
-    </a>
+    </a></li>
   `;
 }
 
