4bb25f14da8434ee33d26a354ee08eb58e7a9c7e
Chuudim <chulanovdima2507@gmail.com>
2023-03-09 17:53:03 +0200

    #2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,28 +1,67 @@
 import { galleryItems } from './gallery-items.js';
-import * as basicLightbox from 'basiclightbox';
-// Change code below this line
+
 const galleryImg = document.querySelector('.gallery');
 galleryImg.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
 
 function createMarkup(galleryItems) {
-    const markUp = galleryItems.map(({ preview, original }) => {
-        return `<div><a href="${original}" class="gallery__item"><img src="${preview}" alt="${description}" class="gallery__image"/></a></div>`;}).join('');
-    return markUp;
+  return galleryItems.map(({ preview, original, description }) => {
+    return `<div><a href="${original}" class="gallery__item"><img src="${preview}" data-source="${original}" alt="${description}" class="gallery__image"/></a></div>`;
+  }).join('');
 }
 
-galleryImg.addEventListener('cick', onGaleryClick);
-function onGaleryClick(event) {
-    event.preventDefault();
+galleryImg.addEventListener('click', onGalleryClick);
 
-    const isImageEl = event.target.classList.contains('gallery__image');
-    if (e.target.nodeName !== 'IMG') {
-        return;
+function onGalleryClick(event) {
+  event.preventDefault();
+
+  const isImageEl = event.target.classList.contains('gallery__image');
+  if (!isImageEl) {
+    return;
+  }
+
+  const imageUrl = event.target.getAttribute('data-source');
+  const instance = basicLightbox.create(`<img src="${imageUrl}">`, {
+    onShow: (instance) => {
+      const img = instance.element().querySelector('img');
+      img.style.maxWidth = '100%';
+      img.style.maxHeight = '100%';
     }
-    
-    const imageUrl = event.target.getAttribute('src');
-    const instance = basicLightbox.create('<img src= "${imageUrl}" width = "800" height = "600" >');
-       instance.show(); 
+  });
+  instance.show();
 }
-
 
 console.log(galleryItems);
+
+
+
+
+
+// import { galleryItems } from './gallery-items.js';
+
+
+// const galleryImg = document.querySelector('.gallery');
+// galleryImg.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+
+// function createMarkup(galleryItems) {
+//     return galleryItems.map(({ preview, original, description }) => {
+//         return `<div><a href="${original}" class="gallery__item"><img src="${preview}" data-source="${original}" alt="${description}" class="gallery__image"/></a></div>`;
+//     }).join('');
+// }
+
+// galleryImg.addEventListener('click', onGalleryClick);
+
+// function onGalleryClick(event) {
+//     event.preventDefault();
+
+//     const isImageEl = event.target.classList.contains('gallery__image');
+//     if (!isImageEl) {
+//         return;
+//     }
+
+//     const imageUrl = event.target.getAttribute('src');
+//     const instance = basicLightbox.create(`<img src="${imageUrl}" width="100%" height="100%">`);
+//     instance.show();
+// }
+
+// console.log(galleryItems);
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+function createGalleryItem({ preview, original, description }) {
+  return `
+    <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+      <div class="gallery__caption">${description}</div>
+    </a>
+  `;
+}
+
+function renderGallery() {
+  const galleryMarkup = galleryItems.map(createGalleryItem).join('');
+  gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+}
+
+renderGallery();
+
+var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250, });
+
